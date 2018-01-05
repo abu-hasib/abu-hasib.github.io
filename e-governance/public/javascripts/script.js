43d28dd60209b2293895f2e7b2289e0f2d7e06d8
ridwanullah <Ridwan Abiola>
2018-01-05 22:53:12 +0100

    some ui changes

--- e-governance/public/javascripts/script.js
+++ e-governance/public/javascripts/script.js
@@ -1,25 +1,2 @@
-import { setTimeout } from "timers";
-
-var images = [];
-var time = 5000;
-var i = 0; //Start point
-
-images[0] = "images/showcase.jpg";
-images[1] = "images/showcase1.jpg";
-images[2] = "images/showcase2.jpg";
-images[3] = "images/showcase3.jpg";
-
-// change images
-function changeImg() {
-  document.slide.src = images[i];
-
-  if (i < images.length - 1) {
-    i++;
-  } else {
-    i = 0;
-  }
-
-  setTimeout("changeImg()", time);
-}
-
-window.onload = changeImg;
+var link = document.querySelectorAll(".dropdown");
+console.log(link);
